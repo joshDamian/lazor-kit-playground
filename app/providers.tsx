ff08b10d66e09ac54580022e3be81a2862345889
Joshua Avoaja <avoajajoshua@gmail.com>
2026-01-13 12:04:04 +0100

    refactor providers module

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,20 +2,26 @@
 
 import { Buffer as Buffer0 } from "buffer";
 import { LazorkitProvider } from "@lazorkit/wallet";
+import React from "react";
+import { Connection } from "@solana/web3.js";
 
 if (typeof window !== "undefined") {
   window.Buffer = window.Buffer || Buffer0;
 }
 
-const CONFIG = {
-  RPC_URL: process.env.NEXT_PUBLIC_SOLANA_RPC_URL!,
-  PORTAL_URL: "https://portal.lazor.sh",
-  PAYMASTER: {
-    paymasterUrl: "https://kora.devnet.lazorkit.com",
-  },
-};
+export const connection = new Connection(
+  process.env.NEXT_PUBLIC_SOLANA_RPC_URL!,
+);
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const CONFIG = {
+    RPC_URL: process.env.NEXT_PUBLIC_SOLANA_RPC_URL!,
+    PORTAL_URL: "https://lazor-kit-portal.vercel.app",
+    PAYMASTER: {
+      paymasterUrl: "https://kora.devnet.lazorkit.com",
+    },
+  };
+
   return (
     <LazorkitProvider
       rpcUrl={CONFIG.RPC_URL}
